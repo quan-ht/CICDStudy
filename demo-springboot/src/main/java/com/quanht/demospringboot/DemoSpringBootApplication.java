ba21c012295b06f1a021eaaf2902f996f88532ee
quanht <quan.ht@qq.com>
2025-08-12 00:17:58 +0800

    feat(demo-springboot): 添加基础的 Spring Boot 项目配置和测试接口
    - 新增 application.yml 配置文件，设置服务器端口为 8100- 修改 DemoSpringBootApplication 类，添加测试用的 REST 接口- 更新 pom.xml，添加 spring-boot-starter-web 依赖

--- demo-springboot/src/main/java/com/quanht/demospringboot/DemoSpringBootApplication.java
+++ demo-springboot/src/main/java/com/quanht/demospringboot/DemoSpringBootApplication.java
@@ -2,12 +2,47 @@ package com.quanht.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+
+	/**
+	 * http://localhost:8100/test
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public ResponseBodyEmitter test(HttpServletResponse response) {
+		response.setContentType("text/event-stream");
+		response.setCharacterEncoding("UTF-8");
+		response.setHeader("Cache-Control", "no-cache");
+
+		ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+
+		String[] words = new String[]{"恭喜，", "你的", "项目", "部", "署", "测", "试", "成", "功", "了！"};
+		new Thread(() -> {
+			for (String word : words) {
+				try {
+					emitter.send(word);
+					Thread.sleep(250);
+				} catch (IOException | InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}).start();
+
+		return emitter;
 	}
 
 }
