e172c1952ab135b9ba40ab83a4e4716b806ce66e
quanht <quan.ht@qq.com>
2025-08-11 23:50:33 +0800

    1

---
+++ demo-springboot/src/test/java/com/quanht/demospringboot/DemoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanht.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
